d2b18c688dd8231542089483de81da59342761d5
chunyangjiang <xwolf@foxmail.com>
2013-11-01 22:41:37 +0800

    before blog

--- app.js
+++ app.js
@@ -8,8 +8,22 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var util = require('util');
 
 var app = express();
+
+
+app.locals({
+	inspect: function(obj){
+		return util.inspect(obj, true);
+	}
+});
+
+app.use(function(req, res, next){
+	res.locals.headers = req.headers;
+	next();
+});
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -30,6 +44,19 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/hello', routes.hello);
+app.get('/list', function(req, res){
+	res.render('list',{
+		title: 'List',
+		items: [1991, 'byvoid','express', 'node.js']
+	});
+});
+
+app.get('/helper', function(req, res){
+	res.render('helper', {
+		title: 'Helpers'
+	});
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
